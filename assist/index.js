e9d44d88d53ee27b16d7d4217ef5416b31b15cd4
lodestar <122153472+Ld-star0395@users.noreply.github.com>
2023-04-03 13:58:51 +0600

    fix mobile hamburger

--- assist/index.js
+++ assist/index.js
@@ -1,13 +1,23 @@
 const handleHeaderButton = () => {
   setTimeout(() => {
-    var dropdown = document.querySelector(".navbar-collapse");
-    var headerButton = document.getElementById("header-navbar-toggle");
+    let dropdown = document.querySelector(".navbar-collapse");
+    let headerButton = document.getElementById("header-navbar-toggle");
+    const  navbar  = document.querySelector("#navbar");
+    const hamburger = document.getElementById("hamburger");
+    const hamburgerCancel = document.getElementById("hamburger-cancel");
+
     console.log("123", dropdown.className);
     if(dropdown.className.includes("show")) {
       headerButton.style.visibility = "hidden";
+      navbar.classList.add("show");
+      hamburger.style.display = "none";
+      hamburgerCancel.style.display = "inline";
     }
     else {
       headerButton.style.visibility = "visible";
+      navbar.classList.remove("show");
+      hamburger.style.display = "inline";
+      hamburgerCancel.style.display = "none";
     }
   }, 400)
 }
